01fb263c53b1849e43c9bc1aa03a80953e0d1800
Agarwal, Pankaj <2005pank@gmail.com>
2020-07-14 11:09:22 +0530

    Lazy Evaluations

---
+++ 6-BeingLazy/src/Chapter6/Evaluation.java
@@ -0,0 +1,43 @@
+package Chapter6;
+
+import java.util.function.Supplier;
+
+public class Evaluation {
+    public static boolean evaluate(final int value){
+        System.out.println("evaluating ..." + value);
+        simulateTimeConsumingOp(2000);
+        return value>100;
+    }
+
+    private static void simulateTimeConsumingOp(final int milliSeconds) {
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public static void eagerEvaluator(final boolean input1,
+                                      final boolean input2){
+        System.out.println("eagerEvaluator called ...");
+        System.out.println("accept ?:" + (input1 && input2));
+    }
+
+    public static void lazyEvaluator(final Supplier<Boolean> input1,
+                                     final Supplier<Boolean> input2){
+        System.out.println("lazyEvaluator called ...");
+        System.out.println("accept ?:" + (input1.get() && input2.get()));
+    }
+
+    public static void main(String[] args) {
+        System.out.println("//" + "START:EAGER_OUTPUT");
+        eagerEvaluator(evaluate(1),evaluate(2));
+        System.out.println("//" + "END:EAGER_OUTPUT");
+
+        System.out.println("//" + "START:LAZY_OUTPUT");
+        lazyEvaluator(()->evaluate(1),()->evaluate(2));
+        System.out.println("//" + "END:LAZY_OUTPUT");
+        
+    }
+}
